e444702d0938509333219917b36722c30a11b8f1
Proxima11 <103930241+Proxima11@users.noreply.github.com>
2023-02-13 16:21:39 +0700

    WEEK1 FINAL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,82 @@
-package Engine;
+import Engine.Object2d;
+import Engine.ShaderProgram;
 import Engine.Window;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
 import org.lwjgl.opengl.GL;
 
-import static org.lwjgl.glfw.GLFW.;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL30.;
+import static org.lwjgl.opengl.GL30.*;
 
 public class Main {
-
-    private Window window = new Window(800,800, "Hello World!");
-
-    public void run(){
-
-        init();
-        loop();
-
-        // Terminate GLFW and free the error callback
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
-    }
-
+    private Window window =
+            new Window
+                    (800,800,"Hello World");
+    private ArrayList<Object2d> objects
+            = new ArrayList<>();
     public void init(){
         window.init();
         GL.createCapabilities();
 
-        // code
-        // ...
+        //code
+        objects.add(new Object2d(
+                Arrays.asList(
+                        //shaderFile lokasi menyesuaikan objectnya
+                        new ShaderProgram.ShaderModuleData
+                                ("resources/shaders/scene.vert"
+                                        , GL_VERTEX_SHADER),
+                        new ShaderProgram.ShaderModuleData
+                                ("resources/shaders/scene.frag"
+                                        , GL_FRAGMENT_SHADER)
+                ),
+                new ArrayList<>(
+                        List.of(
+                                new Vector3f(0.0f,0.5f,0.0f),
+                                new Vector3f(-0.5f,-0.5f,0.0f),
+                                new Vector3f(0.5f,-0.5f,0.0f)
+                        )
+                )
+        ));
     }
-
     public void loop(){
-        while(window.isOpen()){
+        while (window.isOpen()) {
             window.update();
-            glClearColor(0.0f, 0.0f,0.0f,0.0f);
+            glClearColor(0.0f,
+                    0.0f, 0.0f,
+                    0.0f);
             GL.createCapabilities();
-            // code
-            // ...
+
+            //code
+            for(Object2d object: objects){
+                object.draw();
+            }
+
 
             // Restore state
             glDisableVertexAttribArray(0);
-            // Poll for window events
+
+            // Poll for window events.
             // The key callback above will only be
-            // invoked during this call
+            // invoked during this call.
             glfwPollEvents();
         }
     }
+    public void run() {
 
-    public static void main(String[] args){
+        init();
+        loop();
+
+        // Terminate GLFW and
+        // free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+    public static void main(String[] args) {
         new Main().run();
     }
 }
